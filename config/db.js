8d2e96fbf7d308ef645034f433a02366c939f6d4
satishchawali <satishchawali40@gmail.com>
2024-07-05 21:27:08 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,18 @@
-const mongoose = require('mongoose');
-const connectDB = process.env.DATABASE_URL;
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// db.js
 
+import mongoose from 'mongoose';
 
-export default connectDB
+const dbUrl = process.env.DATABASE_URL;
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit process with failure
+    }
+};
