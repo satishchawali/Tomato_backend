007fd4af1419e173bacc608c286c547b1aad4377
satishchawali <satishchawali40@gmail.com>
2024-07-05 21:15:23 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,15 +1,3 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://satishchawali:724893@cluster0.dqjxjvx.mongodb.net/food-del', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log('DB Connected');
-    } catch (error) {
-        console.error('DB Connection Error:', error);
-    }
-}
+const mongoose = require('mongoose');
+const dbUrl = process.env.DATABASE_URL;
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
