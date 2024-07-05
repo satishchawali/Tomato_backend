d10db0f22c2cd16d49a1a5f6125b64a9062cafe4
satishchawali <satishchawali40@gmail.com>
2024-07-05 21:07:34 +0530

    DB changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://satishchawali:724893@cluster0.dqjxjvx.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://satishchawali:724893@cluster0.dqjxjvx.mongodb.net/food-del', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log('DB Connected');
+    } catch (error) {
+        console.error('DB Connection Error:', error);
+    }
 }
