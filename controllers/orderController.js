50d19c0ee6f6f93ee41b3d6f327d9a6c20eda781
satishchawali <satishchawali40@gmail.com>
2024-07-06 09:51:59 +0530

    changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,9 +3,9 @@ import userModel from '../models/userModel.js'
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const frontend_url = "https://tubular-peony-e82490.netlify.app"
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "https://tubular-peony-e82490.netlify.app"
 
     try {
         const newOrder = new orderModel({
@@ -46,8 +46,8 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+            success_url: `${frontend_url}/order/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/order/verify?success=false&orderId=${newOrder._id}`
         });
 
         res.json({ success: true, session_url: session.url });
@@ -61,7 +61,7 @@ const verifyOrder = async (req,res)=>{
     const {orderId,success} = req.body;
 
     try {
-        if(success=="true"){
+        if(success==="true"){
             await orderModel.findByIdAndUpdate(orderId,{payment:true});
             res.json({success:true,message:"Paid"})
         }
